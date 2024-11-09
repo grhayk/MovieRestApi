951c61ac0c70c206959a186e3b92f61f24ad5f4f
Hayk <grigoryan.hayk02@gmail.com>
2024-11-10 00:42:56 +0400

    Initial setup...applying rest principles

---
+++ Movies.Api/Controllers/MoviesController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Movies.Api.Mapping;
+using Movies.Application.Models;
+using Movies.Application.Repositories;
+using Movies.Contracts.Requests;
+using Movies.Contracts.Responses;
+
+namespace Movies.Api.Controllers;
+
+[ApiController]
+public class MoviesController : ControllerBase
+{
+    private readonly IMovieRepository _movieRepository;
+
+    public MoviesController(IMovieRepository movieRepository)
+    {
+        _movieRepository = movieRepository;
+    }
+    
+    [HttpPost(ApiEndpoints.Movies.Create)]
+    public async Task<IActionResult> Create([FromBody] CreateMovieRequest request)
+    {
+        var movie = request.MapToMovie();
+        await _movieRepository.CreateAsync(movie);
+        var movieResponse = movie.MapToMovieResponse();
+        
+        //the right of creation...returning a location of new created item
+        //return Created($"/{ApiEndpoints.Movies.Create}/{movieResponse.Id}", movieResponse);
+        return CreatedAtAction(nameof(Get), new { id = movieResponse.Id }, movieResponse);
+    }
+
+    [HttpGet(ApiEndpoints.Movies.Get)]
+    public async Task<IActionResult> Get([FromRoute] Guid id)
+    {
+        var movie = await _movieRepository.GetByIdAsync(id);
+        if(movie is null) return NotFound();
+        
+        return Ok(movie.MapToMovieResponse());
+    }
+
+    [HttpGet(ApiEndpoints.Movies.GetAll)]
+    public async Task<IActionResult> GetAll()
+    {
+        var movies = await _movieRepository.GetAllAsync();
+        
+        var moviesResponse = movies.MapToMoviesResponse();
+        return Ok(moviesResponse);
+    }
+}
---
+++ Movies.Api/Program.cs
@@ -0,0 +1,25 @@
+using Movies.Application;
+using Movies.Application.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddApplication();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+app.Run();
