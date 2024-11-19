26ed2c68685a54004aba61941dcf6599ddda459b
Hayk <grigoryan.hayk02@gmail.com>
2024-11-19 16:47:01 +0400

    added httpclientfactory to my sdk added authentication with token generation and refreshing also added the remaining requests

--- Movies.Api.Sdk.Consumer/Program.cs
+++ Movies.Api.Sdk.Consumer/Program.cs
@@ -1,9 +1,58 @@
 ﻿using System.Text.Json;
+using Microsoft.Extensions.DependencyInjection;
 using Movies.Api.Sdk;
+using Movies.Api.Sdk.Consumer;
+using Movies.Contracts.Requests;
 using Refit;
 
-var moviesApi = RestService.For<IMoviesApi>("http://localhost:5010");
+//var moviesApi = RestService.For<IMoviesApi>("http://localhost:5010");
 
-var movie = await moviesApi.GetMoviesAsync("first-film-2024");
+var services = new ServiceCollection();
 
-Console.WriteLine(JsonSerializer.Serialize(movie));
+services
+    .AddHttpClient()
+    .AddSingleton<AuthTokenProvider>()
+    .AddRefitClient<IMoviesApi>(x => new RefitSettings()
+    {
+        AuthorizationHeaderValueGetter = async (rq,ct) => await x.GetRequiredService<AuthTokenProvider>().GetTokenAsync()
+    })
+    .ConfigureHttpClient(x =>
+        x.BaseAddress = new Uri("http://localhost:5010"));
+
+var provider = services.BuildServiceProvider();
+
+var moviesApi = provider.GetRequiredService<IMoviesApi>();
+
+var movie = await moviesApi.GetMovieAsync("first-film-2024");
+
+var newMovie = await moviesApi.CreateMovieAsync(new CreateMovieRequest
+{
+    Title = "Spider-Man",
+    YearOfRelease = 2002,
+    Genres = new[]{"Action"}
+});
+
+await moviesApi.UpdateMovieAsync(newMovie.Id, new UpdateMovieRequest
+{
+    Title = "Spider-Man",
+    YearOfRelease = 2002,
+    Genres = new[]{"Action", "Adventure"}
+});
+
+await moviesApi.DeleteMovieAsync(newMovie.Id);
+
+var request = new GetAllMoviesRequest()
+{
+    Title = null,
+    Year = null,
+    SortBy = null,
+    Page = 1,
+    PageSize = 3
+};
+
+var movies = await moviesApi.GetMoviesAsync(request);
+
+foreach (var movieResponse in movies.Items)
+{
+    Console.WriteLine(JsonSerializer.Serialize(movieResponse));
+}
